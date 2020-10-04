90d4f81ac59c961df71e3ed36b13078afe8c76d8
Martin A Valenzuela <martin.valenzuela@bazaarvoice.com>
2020-10-04 16:56:02 -0500

    Clean up Load script
    -Add cleaner Error logging
    -Add some needed references

--- load_csv.py
+++ load_csv.py
@@ -2,21 +2,23 @@ import pandas as pd
 from os import environ
 import logging
 from sqlalchemy import create_engine
+import traceback
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s:%(message)s')
 logger = logging.getLogger(__name__)
 
 
-def main():
+def load_csv():
     # Create the Connection to the Postgres Database
-    alchemy_engine = create_engine('postgresql+psycopg2://%s:%s@127.0.0.1/postgres' % (environ['POSTGRES_USER'],
-                                                                                       environ['POSTGRES_PASSWORD']),
+    alchemy_engine = create_engine('postgresql+psycopg2://%s:%s@%s/postgres' % (environ['POSTGRES_USER'],
+                                                                                       environ['POSTGRES_PASSWORD'],
+                                                                                       environ['POSTGRES_HOST']),
                                    pool_recycle=3600)
     postgres_connection = alchemy_engine.connect()
     table_name = "users"
 
-    # Load User file in to Dataframe
-    users_df = pd.DataFrame('Generated_Data_modified.csv')
+    # Load User file in to a Dataframe
+    users_df = pd.read_csv(environ['CSV_FILE'])
 
     # Split id column in to components
     users_df = pd.concat([users_df,
@@ -25,20 +27,22 @@ def main():
 
     # Set User Id and last four as index
     users_df.set_index(['user_id', 'last_four'], inplace=True)
+    # Set type for date column
+    users_df['visit_date'] = pd.to_datetime(arg=users_df['visit_date'])
 
-    try:
-        users_df[['first_name', 'last_name', 'age', 'gender', 'visit_date']].to_sql(table_name,
-                                                                                    postgres_connection,
-                                                                                    if_exists='replace')
-    except ValueError as e:
-        logger.error(e)
-    except Exception as e:
-        logger.error(e)
-    else:
-        logger.info("Table %s created successfully.", table_name)
-    finally:
-        postgres_connection.close()
+
+    users_df[['first_name', 'last_name', 'age', 'gender', 'visit_date']].to_sql(table_name,
+                                                                                postgres_connection,
+                                                                                if_exists='replace')
+
+    logger.info("Table %s created successfully.", table_name)
+    postgres_connection.close()
+    logger.info("Connection to DB Closed")
+
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        load_csv()
+    except:
+        logger.error(traceback.format_exc())
