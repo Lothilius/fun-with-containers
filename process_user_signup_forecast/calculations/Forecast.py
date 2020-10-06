d98486b9a55b5b09b7e1defd6006248df8228e0d
Martin A Valenzuela <martin.valenzuela@bazaarvoice.com>
2020-10-06 04:25:48 -0500

    Include completed process files
    -Clean up Forecast class
    -Include docker compose file for processing step
    -Fix bad reference in Dockerfile
    -Cleanup and finalize update run counts script

--- process_user_signup_forecast/calculations/Forecast.py
+++ process_user_signup_forecast/calculations/Forecast.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import logging
-import traceback
 import numpy as np
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s:%(message)s')
@@ -9,9 +8,6 @@ logger = logging.getLogger(__name__)
 @pd.api.extensions.register_dataframe_accessor("forecast")
 class ForecastSignup:
     def __init__(self, pandas_obj):
-        """
-        :param pandas_obj:
-        """
         self.validate(pandas_obj)
         self.obj = pandas_obj
         self.datetime_column = self.obj.select_dtypes(include=[np.datetime64]).columns.to_list()[0]
@@ -27,17 +23,20 @@ class ForecastSignup:
             raise Exception("No datetime column found in Dataframe provided")
 
     @property
-    def summary(self):
+    def count_summary(self):
         summary = self.obj.groupby(by=[self.datetime_column]).count()
+        summary.rename(columns={'id': 'count'}, inplace=True)
         return summary
 
 
     @property
     def expanding_mean(self):
+        """ Use summary propery of the dataframe to create cumulative mean column shifted forward a day. The cumulative
+        is the expected users to sign up for the day based on all the previous days.
+        :return: dataframe of the summarized with the count and the cumulative mean column
         """
-        :return:
-        """
-        self.summary['observed'] = self.summary.iloc[:, 0].expanding(min_periods=2).mean()
-        shifted_df = self.summary.shift(1, freq="D")
-        final_df = pd.concat([self.summary, shifted_df], axis=1, sort=False)
-        return final_df
+        shifted_df = self.count_summary.iloc[:, 0].expanding(min_periods=2).mean()
+        shifted_df = shifted_df.shift(1, freq="D")
+        shifted_df.rename('observed', inplace=True)
+        count_mean_df = pd.concat([self.count_summary, shifted_df], axis=1, sort=False)
+        return count_mean_df
--- process_user_signup_forecast/run_update_user_counts.py
+++ process_user_signup_forecast/run_update_user_counts.py
@@ -3,12 +3,17 @@ from os import environ
 from sqlalchemy import create_engine
 import logging
 import traceback
+from calculations.Forecast import ForecastSignup
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s:%(message)s')
 logger = logging.getLogger(__name__)
 
 
 def split_date(date_time_stamp):
+    """ Given a time stamp return a Series with the year month and day components
+    :param date_time_stamp: pandas time stamp
+    :return: Series of the year month and day components of the pandas timestamp
+    """
     assert (type(date_time_stamp) is pd.Timestamp), "A timestamp was not provided"
     return pd.Series({'year': date_time_stamp.year, 'month': date_time_stamp.month, 'day': date_time_stamp.day})
 
@@ -17,7 +22,7 @@ def update_user_counts():
     # Create the Connection to the Postgres Database
     alchemy_engine = create_engine('postgresql+psycopg2://%s:%s@%s/postgres' % (environ['POSTGRES_USER'],
                                                                                 environ['POSTGRES_PASSWORD'],
-                                                                                'localhost'), # environ['POSTGRES_HOST']),
+                                                                                environ['POSTGRES_HOST']),
                                    pool_recycle=3600)
     postgres_connection = alchemy_engine.connect()
 
@@ -27,16 +32,19 @@ def update_user_counts():
     # Restructure ids to one id for uniqueness
     user_df['id'] = user_df[['user_id', 'last_four']].apply(lambda x: str(x[0]) + " " + str(x[1]), axis=1)
 
-    user_df[['id', 'visit_date']].forecast.expanding_mean #.reindex(inplace=True)
-    user_df = pd.concat([user_df, user_df['visit_date'].apply(split_date)], axis=1)
+    # Use the Forecast class to get cumulative mean
+    user_aggregate = user_df[['id', 'visit_date']].forecast.expanding_mean.reset_index()
+
+    # Split dates in to there year month and day components and concatenate them to the dataframe as columns
+    daily_user_counts = pd.concat([user_aggregate, user_aggregate['visit_date'].apply(split_date)], axis=1)
 
     table_name = "daily_user_counts"
     logger.info("Attempting to create table %s", table_name)
 
     # Create Table in Postgres DB
-    results = user_df[['year', 'month', 'day', 'observed', 'count']].to_sql(table_name,
-                                                                            postgres_connection,
-                                                                            if_exists='replace')
+    results = daily_user_counts[['year', 'month', 'day', 'observed', 'count']].to_sql(table_name,
+                                                                                      postgres_connection,
+                                                                                      if_exists='replace')
 
     logger.info("Results: %s", results)
     postgres_connection.close()
@@ -47,6 +55,5 @@ def update_user_counts():
 if __name__ == '__main__':
     try:
         update_user_counts()
-
     except:
         logger.error(traceback.format_exc())
